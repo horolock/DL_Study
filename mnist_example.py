6719afd822b61cd5edd3be33ef0f8fe14c0a4fa2
horolock <horolock96@gmail.com>
2025-01-16 00:59:19 +0900

    implement inference

--- mnist_example.py
+++ mnist_example.py
@@ -1,29 +1,53 @@
 import sys, os
 sys.path.append(os.pardir)      # 부모 디렉터리의 파일을 가져올 수 있도록 설정
 import numpy as np
+import pickle
+
 from dataset.mnist import load_mnist
 from PIL import Image
+from simple_neural_network import softmax, sigmoid
 
 def img_show(img):
     pil_img = Image.fromarray(np.uint8(img))
     pil_img.show()
 
-# It takes time at first time
-(x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+def get_data():
+    (x_train, t_train), (x_test, t_test) = load_mnist(flatten=True, normalize=False)
+    print(f"x_train shape : {x_train.shape}")
+    print(f"t_train shape : {t_train.shape}")
+    return x_test, t_test
 
-print(x_train.shape)
-print(t_train.shape)
+def init_network():
+    with open("sample_weight.pkl", 'rb') as f:
+        network = pickle.load(f)
+    
+    return network
 
-print(x_test.shape)
-print(t_test.shape)
+def predict(network, x):
+    w1, w2, w3 = network['W1'], network['W2'], network['W3']
+    b1, b2, b3 = network['b1'], network['b2'], network['b3']
 
-img = x_train[0]
-label = t_train[0]
+    a1 = np.dot(x, w1) + b1
+    z1 = sigmoid(a1)
+    a2 = np.dot(z1, w2) + b2
+    z2 = sigmoid(a2)
+    a3 = np.dot(z2, w3) + b3
+    y = softmax(a3)
 
-print(label)
+    return y
 
-print(img.shape)
-img = img.reshape(28, 28)
-print(img.shape)
+########
+# Test
+########
+x, t = get_data()
+network = init_network()
 
-img_show(img)
+accuracy_cnt = 0
+
+for i in range(len(x)):
+    y = predict(network, x[i])
+    p = np.argmax(y)    # Get largest percentage element's index
+    if p == t[i]:
+        accuracy_cnt += 1
+
+print("Accuracy : " + str(float(accuracy_cnt) / len(x)))
