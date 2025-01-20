cdd4447b8f4f321722d3672cffe42817be069e40
horolock <horolock96@gmail.com>
2025-01-20 22:56:46 +0900

    add some comment for refactored section

--- utils/gradient.py
+++ utils/gradient.py
@@ -14,6 +14,7 @@ def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)         # Create array that shape is same as 'x'
 
+    # Edited for multi dimension array
     it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
 
     while not it.finished:
