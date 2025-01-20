d3b0be7b6bc083f82592d6c15280fe755400aa8f
horolock <horolock96@gmail.com>
2025-01-20 22:54:49 +0900

    refactor numerical gradient

--- utils/gradient.py
+++ utils/gradient.py
@@ -14,19 +14,20 @@ def numerical_gradient(f, x):
     h = 1e-4
     grad = np.zeros_like(x)         # Create array that shape is same as 'x'
 
-    for idx in range(x.size):
+    it = np.nditer(x, flags=['multi_index'], op_flags=['readwrite'])
+
+    while not it.finished:
+        idx = it.multi_index
         tmp_val = x[idx]
+        x[idx] = float(tmp_val) + h
+        fxh1 = f(x)         # f(x + h)
 
-        # Calculate f(x+h)
-        x[idx] = tmp_val + h
-        fxh1 = f(x)
-
-        # Calculate f(x-h)
         x[idx] = tmp_val - h
-        fxh2 = f(x)
+        fxh2 = f(x)         # f(x - h)
+        grad[idx] = (fxh1 - fxh2) / (2 * h)
 
-        grad[idx] = (fxh1 - fxh2) / (2*h)
-        x[idx] = tmp_val 
+        x[idx] = tmp_val                # recover value
+        it.iternext()
     
     return grad
 
