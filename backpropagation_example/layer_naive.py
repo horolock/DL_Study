54cb2808766419da0c220ec5504b6298850d554b
horolock <horolock96@gmail.com>
2025-02-08 16:26:29 +0900

    Create add layer

--- backpropagation_example/layer_naive.py
+++ backpropagation_example/layer_naive.py
@@ -25,4 +25,19 @@ class MulLayer:
         dy = dout * self.x
 
         return dx, dy
+
+
+# Add layer
+class AddLayer:
+    def __init__(self):
+        pass
+
+    def forward(self, x, y):
+        out = x + y
+        return out
     
+    def backward(self, dout):
+        dx = dout * 1
+        dy = dout * 1
+
+        return dx, dy
