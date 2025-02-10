8543d87987413df1e17260c95fd44c3699c431a4
horolock <horolock96@gmail.com>
2025-02-10 23:19:23 +0900

    Softmax with Loss

--- backpropagation_example/layers.py
+++ backpropagation_example/layers.py
@@ -54,6 +54,24 @@ class Affine:
         
         return dx
 
+
+class SoftmaxWithLoss:
+    def __init__(self):
+        self.loss = None
+        self.y = None
+        self.x = None
+
+    def forward(self, x, t):
+        self.t = t
+        self.y = softmax(x)
+        self.loss = cross_entropy_error(self.y, self.t)
+        return self.loss
+    
+    def backward(self, dout=1):
+        batch_size = self.t.shape[0]
+        dx = (self.y - self.t) / batch_size
+        return dx
+
 # Example
 x = np.array([[1.0, -0.5], [-2.0, 3.0]])
 print(x)
