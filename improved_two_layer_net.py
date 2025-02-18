d67e635fbf3de7e073dc48e06b3a3cbf5cf9acca
horolock <horolock96@gmail.com>
2025-02-18 23:50:21 +0900

    implement initialize part

--- improved_two_layer_net.py
+++ improved_two_layer_net.py
@@ -12,6 +12,16 @@ class TwoLayerNet:
         # Initialize weights
         self.params = {}
         self.params['W1'] = weight_init_std * np.random.randn(input_size, hidden_size)
+        self.params['b1'] = np.zeros(hidden_size)
+        self.params['W2'] = weight_init_std * np.random.randn(hidden_size, output_size)
+        self.params['b2'] = np.zeros(output_size)
+
+        # Create Layer
+        self.layers = OrderedDict()
+        self.layers['Affine1'] = Affine(self.params['W1'], self.params['b1'])
+        self.layers['Relu1'] = Relu()
+        self.layers['Affine2'] = Affine(self.params['W2'], self.params['b2'])
+        self.lastLayer = SoftmaxWithLoss()
 
     # Process predict
     # x - Image data
